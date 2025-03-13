dd6504fde3a6e94a23a33775862abe4b8f5d5ff6
Phillip Emmons <phillip.emmons@gmail.com>
2025-03-12 23:46:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/hello')
+def hello():
+    return jsonify({"message": "Hello from my new route!"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
