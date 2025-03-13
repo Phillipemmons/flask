a135b6467148c66feffd78e7b9b9627d961e05f9
Phillip Emmons <phillip.emmons@gmail.com>
2025-03-13 00:16:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,5 @@ def index():
 def hello():
     return jsonify({"message": "Hello from my new route!"})
 
-
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
